c6f8694b0b2966b0e9ca7c251df05955c0c27185
CROIRE-Aline <niyocroirealine@gmail.com>
2024-02-21 15:21:27 +0200

    adding a function to display sorted tasks

--- scheduler.js
+++ scheduler.js
@@ -11,5 +11,14 @@ const addTask=(titel,descrption,duedate)=>{
     }
     tasks.push(task);
 }
+//calling addTask function
 addTask('study','learning algorithm',Date());
 console.log(tasks);
+//function to display tasks sorted by duedates
+const displaytasks=()=>{
+    let sortedTasks=tasks.sort((a,b)=>new Date(a.duedate)-new Date(b.duedate));
+    console.log(sortedTasks);
+}
+addTask('leisure','trip to musanze','2023-12-9');
+addTask('meeting','school parents meeting','2024-2-13');
+displaytasks();
